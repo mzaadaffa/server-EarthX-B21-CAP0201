7a40b7ab612d4723d85d3b5cdf56914f111b3ff6
mzaadaffa <45386200+mzaadaffa@users.noreply.github.com>
2021-05-23 20:18:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ user_col = db['users']
 
 BASE_URL = "/api/v1"
 
-
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
@@ -57,160 +56,18 @@ def token_required(f):
     return decorated
 
 
+
+from services.auth import auth_bp
+app.register_blueprint(auth_bp, url_prefix=BASE_URL+'/authentication')
+
+from services.analytics import analytics_bp
+app.register_blueprint(analytics_bp, url_prefix=BASE_URL+'/analytics')
+
+
+
 @app.route("/")
 def index():
     return "Hello World"
-
-
-@app.route(BASE_URL+"/authentication/signout", methods=['POST'])
-@token_required
-def signout(current_user):
-    try:
-        query = {"email": current_user}
-        update_query = {"$set": {"token": ""}}
-        user_col.update_one(query, update_query)
-        response = app.response_class(
-            response=json.dumps({
-                "message": 'success',
-                "status": 200,
-            }),
-            status=200,
-            mimetype='application/json'
-        )
-        return response
-    except Exception as e:
-        print(e)
-        response = app.response_class(
-            response=json.dumps({
-                "message": 'something wrong'
-            }),
-            status=422,
-            mimetype='application/json'
-        )
-        return response
-
-
-@app.route(BASE_URL+"/authentication/signup", methods=['POST'])
-def signup():
-    try:
-        email = request.form.get("email")
-        password = request.form.get("password")
-        salt = bcrypt.gensalt()
-        if(is_email_available(email)):
-            hashed_password = bcrypt.hashpw(password.encode('utf8'), salt)
-            user = {"email": email,
-                    "password": hashed_password, "token": ""}
-            user_data = user_col.insert_one(user)
-            response = app.response_class(
-                response=json.dumps({
-                    "message": 'success',
-                    "status": 200,
-                }),
-                status=200,
-                mimetype='application/json'
-            )
-            return response
-
-        else:
-            response = app.response_class(
-                response=json.dumps({
-                    "message": 'already exist',
-                    "status": 403,
-                }),
-                status=403,
-                mimetype='application/json'
-            )
-            return response
-    except Exception as e:
-        print(e)
-        response = app.response_class(
-            response=json.dumps({
-                "message": 'something wrong',
-                "status": 422,
-            }),
-            status=422,
-            mimetype='application/json'
-        )
-        return response
-
-
-@app.route(BASE_URL+"/authentication/signin", methods=['POST'])
-def signin():
-    try:
-        email = request.form.get("email")
-        password = request.form.get("password")
-        query = {"email": email}
-        mydoc = list(user_col.find(query))
-        if(len(mydoc) > 0):
-            password_user = mydoc[0]['password']
-            hashed_password = bcrypt.checkpw(
-                password.encode('utf-8'), password_user)
-
-            if(hashed_password):
-                if(mydoc[0]['token'] == ""):
-                    token = jwt.encode({
-                        'email': email
-                    }, app.config['SECRET_KEY'])
-                    update_query = {"$set": {"token": token}}
-
-                    user_col.update_one(query, update_query)
-                    response = app.response_class(
-                        response=json.dumps({
-                            "message": 'success',
-                            "status": 200,
-                            "user": {
-                                "id": str(mydoc[0]['_id']),
-                                "email": email,
-                                "token": token.decode('utf-8')
-                            }
-                        }),
-                        status=200,
-                        mimetype='application/json'
-                    )
-                    return response
-                else:
-                    response = app.response_class(
-                        response=json.dumps({
-                            "message": 'already login',
-                            "status": 403,
-                        }),
-                        status=403,
-                        mimetype='application/json'
-                    )
-                    return response
-
-            else:
-                response = app.response_class(
-                    response=json.dumps({
-                        "message": 'wrong password',
-                        "status": 403,
-                    }),
-                    status=403,
-                    mimetype='application/json'
-                )
-                return response
-
-        else:
-            response = app.response_class(
-                response=json.dumps({
-                    "message": 'email does not exist',
-                    "status": 403,
-                }),
-                status=403,
-                mimetype='application/json'
-            )
-            return response
-    except Exception as e:
-        print(e)
-        response = app.response_class(
-            response=json.dumps({
-                "message": 'something wrong',
-                "status": 422,
-            }),
-            status=422,
-            mimetype='application/json'
-        )
-        return response
 
 
 @app.route(BASE_URL+"/user/dummy")
@@ -251,16 +108,7 @@ def delete_all_user():
     return "Deleted all user"
 
 
-def is_email_available(email):
-    query = {"email": email}
-    mydoc = list(user_col.find(query))
-    if(len(mydoc) > 0):
-        return False
-    else:
-        return True
-
 # decorator for verifying the JWT
-
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=3000)
