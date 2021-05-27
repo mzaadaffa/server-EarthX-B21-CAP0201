97101ac6d2e609ad8a9deae98bedc6c55b960995
mzaadaffa <45386200+mzaadaffa@users.noreply.github.com>
2021-05-27 15:34:59 +0700

    Update analytics.py

--- services/analytics.py
+++ services/analytics.py
@@ -6,12 +6,24 @@ import json
 from datetime import date 
 
 
-
+history_col = db['history']
 analytics_bp = Blueprint('analytics', __name__)
 
-@token_required
+#ENUM FOR CATEGORY
+#Brand, Movie, Video Game, Musician, Actor
+
+enum = {
+    "Brand":47,
+    "Movie":86,
+    "Video Game":71,
+    "Musician":54,
+    "Actor":55
+}
+
+
 @analytics_bp.route("/top-10/generate", methods=['POST'])
-def generateTop10():
+@token_required
+def generateTop10(current_user):
     try:
         keyword = request.form.get("keyword")
         hashtag = request.form.get("hashtag")
@@ -22,7 +34,42 @@ def generateTop10():
         is_realtime = request.form.get("is_realtime")
         date_start = request.form.get("date_start")
         date_end = request.form.get("date_end")
+
+    
+      
+
+        # TODO
+        # CALL ML SERVICE FUNCTION
+        # REMOVE SLEEP
         time.sleep(10)
+
+
+
+        #TODO
+        #GET DATA RESULT FROM ML SERVICE
+        #CHANGE THE ACTUAL RESULT
+        
+        history = {
+            "user":current_user,
+            "date_created": str(date.today()),
+            "type":"Top 10 Graph",
+            "keyword":keyword,
+            "hashtag":hashtag,
+            "category":category,
+            "language":language,
+            "location":location,
+            "is_retweeted":is_retweeted,
+            "is_realtime":is_realtime,
+            "date_start":str(date_start),
+            "date_end": str(date_end),
+            "result": []
+        }
+      
+        history_data = history_col.insert_one(history)
+        
+
+        # TODO 
+        # CHANGE WITH ACTUAL RESULT WITH SAME STRUCTURE
         return app.response_class(
                 response=json.dumps({
                     "message": 'success',
@@ -76,12 +123,38 @@ def generateTop10():
 
 
 
-@token_required
+
 @analytics_bp.route("/sentiment/generate", methods=['POST'])
-def generateSentiment():
+@token_required
+def generateSentiment(current_user):
     try:
         keyword = request.form.get("keyword")
+       
+
+        # TODO
+        # CALL ML SERVICE FUNCTION
+        # REMOVE SLEEP
         time.sleep(10)
+
+
+
+        #TODO
+        #GET DATA RESULT FROM ML SERVICE
+        #CHANGE THE ACTUAL RESULT
+        
+        history = {
+            "user":current_user,
+            "date_created": str(date.today()),
+            "type":"Sentiment Analysis",
+            "keyword":keyword,
+            "result": {}
+        }
+      
+        history_data = history_col.insert_one(history)
+        
+
+        #TODO
+        #CHANGE THE ACTUAL DATA RESULT
         return app.response_class(
                 response=json.dumps({
                     "message": 'success',
@@ -108,48 +181,28 @@ def generateSentiment():
         return response
 
 
-@token_required
 @analytics_bp.route("/history", methods=['GET'])
-def viewHistory():
+@token_required
+def viewHistory(current_user):
     try:
+        query = {"user" : current_user}
+        mylist = list(history_col.find(query))
+        result_list = []
+        if(len(mylist)>10):
+            result_list = mylist[0:10]
+        else:
+            result_list = mylist
+        
+
+        for i in range(0,len(result_list)):
+            temp_list = result_list[i]
+            temp_list["_id"] = str(temp_list["_id"])
+            result_list[i] = temp_list
+        
         return app.response_class(
                 response=json.dumps({
                     "message": 'success',
-                    "data":[
-                        {
-                            "timestamp":str(date.today()),
-                            "type":"sentiment",
-                            "topic":"ini keyword yang dicari",
-
-
-                        },
-                         {
-                           "timestamp":str(date.today()),
-                            "type":"top10",
-                            "topic":"ini keyword yang dicari",
-
-                        },
-                          {
-                           "timestamp":str(date.today()),
-                            "type":"top10",
-                            "topic":"ini keyword yang dicari",
-
-                        },
-                         {
-                           "timestamp":str(date.today()),
-                            "type":"top10",
-                            "topic":"ini keyword yang dicari",
-
-                        },
-                         {
-                            "timestamp":str(date.today()),
-                            "type":"sentiment",
-                            "topic":"ini keyword yang dicari",
-
-
-                        },
-                        
-                    ],
+                    "data":result_list,
                     "status": 200,
                 }),
                 status=200,
